f564b7b28e31fd3ece6e0189331188b2a610aab2
Andrey Chekmarev <assminog@gmail.com>
2021-04-01 18:16:14 +0300

    Add files via upload

---
+++ Start.php
@@ -0,0 +1,79 @@
+#!/usr/bin/php
+<?php                  /*_
+© Andrey Chekmaryov 2021
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Email:    assminog@gmail.com,|Telegram: https://t.me/HficSamin  |VK:     	https://vk.com/Hfic.Samin
+	  tubmulur@yandex.ru |				        | 		https://vk.com/HiFiIntelligentClub
+Phone:    +7(911)787-44-57,  |Whatsapp: +7(911)787-44-57	|Facebook: 	https://facebook.com/Hfic.Samin	
+			     |					|		https://facebook.com/HiFiIntelligentClub
+                             |					| 
+Site[Ru] Public browsing international:  http://HiFiIntelligentClub.Ru  <- Not workimg out of money. Registered untill
+Site[En] Public browsing international:  http://HiFiIntelligentClub.COM <- Not workimg out of money.
+Site[En] Private browsing international: http://ryklzxobxv4s32omimbu7d7t3cdw6dplvsz36zsqqu7ad2foo5m3tmad.onion <- Free of charge,not working.
+														  Will be started soon for
+        	        													  listening with pleasure.
+														  International streams 
+	        													  divided by categories 
+														  described as ICQR.
+			    |E    |D     |R      |O      |
+||||||||||||||||||||||||||||Event|Design|Reality|Objects||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+ /////
+  //
+/////
+function сВремя(){return round(microtime(true), 4);}
+$сКиМ		="/home/EDRO.SetOfTools/System/-1.КиМ/0.КиМ.php";
+if(file_get_contents($сКиМ)===FALSE)
+	{exit;
+	}
+else
+	{
+	require$сКиМ;
+	echo 'КиМ загружен'."\n";
+	}
+/////////////////////////////////////////////////////////////////////////////////////////  ////// /
+$оКиМ			= КиМ::оНачПроц('Загрузить библиотеки');				////////  //////  /
+$оКиМ->ч0ЖдёмЧастей	= 16;							    //      //  /
+$оКиМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/0.Loader/0.loader.php';	   //      /////// /
+			$оКиМ->сНач();						  //      ////// /
+			$оКиМ->_КонПроц();					///////
+////////////////////////////////////////////////////////////////////////////////////////
+$оКиМ			= КиМ::оНачПроц('Выставить точку входа');
+$оКиМ			= КиМ::оНачПроц('Собрать объект ЕДРО:ПОЛИМЕР');
+//$oEDRO	=new EDRO();
+//print_r($oEDRO);
+exit;
+////////////////////////////////////////////////////////////////////////////////////////
+//$оКиМ			= КиМ::оНачПроц('Set text broadcast proxy');	///		//
+//$оКиМ->ч0ЖдёмЧастей	= 1;							//   //
+//$оКиМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/8.Вещание/0.Prepare.php';	//   //
+//			$оКиМ->_КонПроц();				//		//
+/////////////////////////////////////////////////////////////////////////////////////////
+				    				//	LL 
+											//
+											//
+											//
+			///////////							//
+			 // // //							//
+			 // // //							//
+			///////////							
+
+//$оКиМ			= КиМ::оНачПроц('Listener provider to HIC sound amusement conveer line.');
+
+
+
+
+
+$оКиМ			= new КиМ('Собрать EDRO');
+$оКиМ->oE		= new Event();
+
+//$оКиМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/5.Styles/0.CSS.Styles.php';
+//$strStyles		= $оКиМ->сНач();
+			$оКиМ->_КонПроц();
+
+//$оКиМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/6.HTML_Interfaces/0.HTML_HeadInterface.php';
+//			$оКиМ->сНач();
+//			$оКиМ->_КонПроц();
+
+//			$оКиМ->_КонПроц();
+?>
