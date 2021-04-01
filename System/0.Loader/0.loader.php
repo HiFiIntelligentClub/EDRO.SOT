0016bc091850bbe0032dda16f2b6c00d32f75736
Andrey Chekmarev <assminog@gmail.com>
2021-04-01 18:21:27 +0300

    Add files via upload

--- System/0.Loader/0.loader.php
+++ System/0.Loader/0.loader.php
@@ -4,84 +4,78 @@
 
 //while($мЗагрузка=фКИМ($мЗагрузка))
 //	{
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/0.ReportError.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/0.key.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/1.Report.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/0.ReportError.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/2.ReportComplain_Violation_CitizenCountry1_Country2_.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/1.Report.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/2.ReportComplain_Violation_УКРФ.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/2.ReportComplain_Violation_CitizenCountry1_Country2_.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/2.ReportViloationLaw_ResidenceCountry.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/2.ReportComplain_Violation_УКРФ.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/2.ReportViolation_ResourceRules.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/2.ReportViloationLaw_ResidenceCountry.php';
+$str			= $this->сНач();
+
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Reporter/2.ReportViolation_ResourceRules.php';
+$str			= $this->сНач();
 /*
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Оповещение/0.ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SendMail/';
-			$оКИМ->_Нач();
-			//require($оКИМ->сИмяФайла);
-
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Оповещение/0.ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Complains';
-			$оКИМ->_Нач();
-			//require($оКИМ->сИмяФайла);
-
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Оповещение/0.ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/ResidenceCountryLaw';
-			$оКИМ->_Нач();
-			//require($оКИМ->сИмяФайла);
-
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Оповещение/0.ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/ResurceRules';
-			$оКИМ->_Нач();
-			//require($оКИМ->сИмяФайла);
-
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Оповещение/0.ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/Rights_ResidentCounttry1_residentCountry2';
-			$оКИМ->_Нач();
-			//require($оКИМ->сИмяФайла);
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Оповещение/0.ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SendMail/';
+			$this->_Нач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Оповещение/0.ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Complains';
+			$this->_Нач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Оповещение/0.ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/ResidenceCountryLaw';
+			$this->_Нач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Оповещение/0.ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/ResurceRules';
+			$this->_Нач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/1.Оповещение/0.ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/Rights_ResidentCounttry1_residentCountry2';
+			$this->_Нач();
 */
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/1.FunctionsSetup.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/0.key.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/2.Platforms.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/1.FunctionsSetup.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/3.Functions.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/2.Platforms.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/4.GoodCatJS.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/3.Functions.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/4.Фразы.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/4.Фразы.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/5.Dyn.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/5.Dyn.php';
-$str			= $оКИМ->сНач();
-
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/6.StringFunctions.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/2.Functions/6.StringFunctions.php';
+$str			= $this->сНач();
 
 /*
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/3.VectorKIIM/0.KIIM.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/3.VectorKIIM/0.KIIM.php';
+$str			= $this->сНач();
 
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/3.VectorKIIM/1.objKIIM.activation.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/3.VectorKIIM/1.objKIIM.activation.php';
+$str			= $this->сНач();
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/3.VectorKIIM_Helper/1.objKIIM.activation.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/3.VectorKIIM_Helper/1.objKIIM.activation.php';
+$str			= $this->сНач();
 */
 
-$оКИМ->сИмяФайла	= '/home/EDRO.SetOfTools/System/4.RAMBuffer/0.EDRO_Objects.php';
-$str			= $оКИМ->сНач();
+$this->сИмяФайла	= '/home/EDRO.SetOfTools/System/4.RAMBuffer/0.EDRO_Objects.php';
+$str			= $this->сНач();
+
 	
 	//$ч0Шаг++;
 //	}
 
-$оКИМ->ч0ВыполненоЧастей++;
-$оКИМ->_Кон();
+$this->ч0ВыполненоЧастей++;
+$this->_Кон();
 ?>
